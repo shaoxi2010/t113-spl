553e2a77188107aabfc79b76858e10a996bb92f9
shaoxi2010 <shaoxi2010@qq.com>
2025-02-19 00:30:05 +0800

    hack from d1

---
+++ include/config.h
@@ -0,0 +1,27 @@
+#ifndef __Users_zhaoshaoxi_CSpace_spl_include_config_h_
+#define __Users_zhaoshaoxi_CSpace_spl_include_config_h_
+#include<sun20iw1p1.h>
+#define CFG_AXP1530_POWER 1
+#define CFG_BOOT0_RUN_ADDR 0x20000
+#define CFG_FES1_RUN_ADDR 0x28000
+#define CFG_LICHEE_BOARD CFG_
+#define CFG_SBOOT_RUN_ADDR 0x20480
+#define CFG_SPINOR_UBOOT_OFFSET 128
+#define CFG_SPI_USE_DMA 1
+#define CFG_SUNXI_AUTO_TWI 1
+#define CFG_SUNXI_CHIPID 1
+#define CFG_SUNXI_DMA 1
+#define CFG_SUNXI_GPIO_V2 1
+#define CFG_SUNXI_GUNZIP 1
+#define CFG_SUNXI_LZ4 1
+#define CFG_SUNXI_LZMA 1
+#define CFG_SUNXI_MEMOP 1
+#define CFG_SUNXI_PMIC 1
+#define CFG_SUNXI_POWER 1
+#define CFG_SUNXI_SPI 1
+#define CFG_SUNXI_SPINOR 1
+#define CFG_SUNXI_TWI 1
+#define CFG_SYS_INIT_RAM_SIZE 0x10000
+#define CFG_USE_DCACHE 1
+#define PLATFORM_LIBGCC -L /usr/lib/gcc-cross/arm-linux-gnueabi/12 -lgcc
+#endif
